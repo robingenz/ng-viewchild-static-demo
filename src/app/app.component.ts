0c8379561e14a68648f3617354a2aaac295660b4
Robin Genz <mail@robingenz.dev>
2021-01-18 11:12:10 +0100

    feat: add demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ContentChild, ElementRef, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-viewchild-static-demo';
+export class AppComponent implements OnInit, AfterViewInit , AfterContentInit{
+  showButton = true;
+  /**
+   * Dynamic queries: The results of dynamicly labelled query results depend on runtime values like bindings.
+   * So they are not available after change detection. So they are accessible in ngAfterViewInit or ngAfterContentInit.
+   * @see https://www.thecodecampus.de/blog/angular-viewchild-static-property-in-ng8/
+   */
+  @ViewChild('button', { static: false }) viewChildButtonStaticFalse: ElementRef | undefined;
+  /**
+   * Static queries: Result of the query is determined statically and does not depend on runtime values.
+   * Query results of this kind are available before Change Detection and are accessible in ngOnInit.
+   * @see https://www.thecodecampus.de/blog/angular-viewchild-static-property-in-ng8/
+   */
+  @ViewChild('button', { static: true }) viewChildButtonStaticTrue: ElementRef | undefined;
+
+  public ngOnInit(): void {
+    console.log('[ngOnInit]', { staticFalse: this.viewChildButtonStaticFalse });
+    console.log('[ngOnInit]', { staticTrue: this.viewChildButtonStaticTrue });
+  }
+
+  public ngAfterViewInit(): void {
+    console.log('[ngAfterViewInit]', { staticFalse: this.viewChildButtonStaticFalse });
+    console.log('[ngAfterViewInit]', { staticTrue: this.viewChildButtonStaticTrue });
+  }
+
+  public ngAfterContentInit(): void {
+    console.log('[ngAfterContentInit]', { staticFalse: this.viewChildButtonStaticFalse });
+    console.log('[ngAfterContentInit]', { staticTrue: this.viewChildButtonStaticTrue });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
